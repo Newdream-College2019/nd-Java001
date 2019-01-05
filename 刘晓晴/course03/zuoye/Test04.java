a7feee220b756c2cffc218ace1dd1c59505006c2
Leelxq <913827616@qq.com>
2019-01-05 11:40:18 +0800

    第三章作业

---
+++ 刘晓晴/course03/zuoye/Test04.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Test04{
+	public static void main(String[] args){
+		Scanner input=new Scanner(System.in);
+		System.out.print("请问你爱我吗？(yes/no/..)");
+		String answer=input.next();
+		if(answer.equals("yes")){
+			System.out.println("你是爱我的");
+		}else if(answer.equals("no")){
+			System.out.println("原来你不爱我");
+		}else{
+			System.out.println("你到底是爱还是不爱，自己也不清楚");
+		}
+	}
+}

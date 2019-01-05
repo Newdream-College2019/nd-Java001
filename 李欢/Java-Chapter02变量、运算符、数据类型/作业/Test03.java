556b08eaf7fa34a173f89d303c8455ee6b4a4a6e
visionary7 <1107331058@qq.com>
2019-01-05 11:00:13 +0800

    1，第一次提交

---
+++ 李欢/Java-Chapter02变量、运算符、数据类型/作业/Test03.java
@@ -0,0 +1,12 @@
+public class Test03{
+	public static void main(String[] args){
+		int day=10;
+		int hour;
+		int mm;
+		hour=day*24;
+		mm=day*24*60;
+		System.out.println("天\t小时\t分钟");
+		System.out.println(day+"\t"+hour+"\t"+mm);
+
+	}
+}

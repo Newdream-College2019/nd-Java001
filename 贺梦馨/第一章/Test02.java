8676c65aff1c77ec27c463cb6f64951fe4d0e961
00danny <916496993@qq.com>
2019-01-05 10:54:36 +0800

    第一次提交

---
+++ 贺梦馨/第一章/Test02.java
@@ -0,0 +1,5 @@
+public class Test02{
+	public static void main(String[] args){
+		System.out.print("我的名字：小样\n我的性别：男\n我的年龄：18");
+}
+}
---
+++ 贺梦馨/第一章/Test07.java
@@ -0,0 +1,11 @@
+public class Test07{
+	public static void main(String[] args){
+		System.out.println("日\t"+"一\t"+"二\t"+"三\t"+"四\t"+"五\t"+"六");
+		System.out.println("\t"+"\t"+"\t"+"1\t"+"2\t"+"3\t"+"4");
+		System.out.println("5\t"+"6\t"+"7\t"+"8\t"+"9\t"+"1O\t"+"11");
+		System.out.println("12\t"+"13\t"+"14\t"+"15\t"+"16\t"+"17\t"+"18");
+		System.out.println("19\t"+"20\t"+"21\t"+"22\t"+"23\t"+"24\t"+"25");
+		System.out.println("26\t"+"27\t"+"28\t"+"30\t"+"\t"+"\t"+"");
+		
+}
+}

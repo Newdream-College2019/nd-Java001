5896c924f0e320b1d6bc884d12acc124101a8902
visionary7 <1107331058@qq.com>
2019-01-16 20:42:14 +0800

    第十章作业

---
+++ 李欢/Java-Chapter10/8/Monster.java
@@ -0,0 +1,9 @@
+package chapter10;
+public class Monster {
+	String name;
+	int life;
+	public void show(){
+		System.out.println("怪物姓名："+name);
+		System.out.println("生命值："+life);
+	}
+}
---
+++ 李欢/Java-Chapter10/8/Weapon.java
@@ -0,0 +1,10 @@
+package chapter10;
+
+public class Weapon {
+	String name;
+	int atk;
+	public void show(){
+		System.out.println("ÎäÆ÷ĞÕÃû£º"+name);
+		System.out.println("¹¥»÷Á¦£º"+atk);
+	}
+}
